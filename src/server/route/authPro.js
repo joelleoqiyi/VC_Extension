93e9c3b382e1031fd605e09341df421a29e61548
Joel Leo <leo.qiyi.joel@dhs.sg>
2020-06-22 12:51:58 +0800

    fixed CORS bugs

--- src/server/route/authPro.js
+++ src/server/route/authPro.js
@@ -10,7 +10,7 @@ var authPro = express.Router()
 var cors = require('cors')
 
 //setting up CORS settings
-var whitelist = ['https://vcxtension-website.herokuapp.com/', 'http://localhost:1234']
+var whitelist = ['https://vcxtension-website.herokuapp.com', 'http://localhost:1234']
 var corsOptions = {
   origin: function (origin, callback) {
     if (whitelist.indexOf(origin) !== -1) {
--- src/server/route/dataPro.js
+++ src/server/route/dataPro.js
@@ -9,7 +9,7 @@ var dataPro = express.Router()
 var cors = require('cors')
 
 //setting up CORS settings
-var whitelist = ['https://vcxtension-website.herokuapp.com/', 'http://localhost:1234']
+var whitelist = ['https://vcxtension-website.herokuapp.com', 'http://localhost:1234']
 var corsOptions = {
   origin: function (origin, callback) {
     if (whitelist.indexOf(origin) !== -1) {
